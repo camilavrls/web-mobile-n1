35a13b663624b41ffd65d3b30d43cd646e9caa17
Camila Sousa <camilamorgada7@gmail.com>
2024-04-11 00:13:27 -0300

    estrutura projeto

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,72 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import "./globals.css";
+import { Toolbar, IconButton, TextField } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
-export default function Home() {
+export default function App() {
+  return(
+    <div>
+      <Home></Home>
+    </div>
+  );
+}
+
+function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <section className="Principal">
+      <div className="head">
+        <Toolbar>
+          <IconButton>
+            <MenuIcon />
+          </IconButton>
+          <SearchBar />
+          <Botao nome="Conheç mais sobre a gente"/>
+        </Toolbar>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="div1">
+        <h1>O QUE BUSCAMOS? </h1>
+        <p> Buscamos criar um espaço acolhedor e inclusivo, onde mulheres na tecnologia possam encontrar apoio mútuo e solidariedade. Queremos demonstrar que cada desafio enfrentado é uma oportunidade de crescimento, e que juntas, podemos superar barreiras e alcançar nossos objetivos. Aqui, celebramos a diversidade de habilidades e experiências, reconhecendo o valor único que cada uma traz para a comunidade. Seja para compartilhar conhecimento, buscar orientação ou simplesmente encontrar um ombro amigo, estamos comprometidas em fortalecer e empoderar umas às outras. Porque acreditamos firmemente que, unidas, somos capazes de conquistar o mundo da tecnologia e inspirar as gerações futuras.</p>
+
+        <Botao nome="Conhecer mais sobre a gente"/>
+      </div>
+      <div className="div2">
+        <h1>ELAS NA CIÊNCIA </h1>
+        <p> Marie Skłodowska-Curie, foi uma física e química polonesa naturalizada francesa, que conduziu pesquisas pioneiras sobre radioatividade. Ela foi a primeira mulher a ganhar o Prêmio Nobel, sendo também a primeira pessoa e a única mulher a ganhá-lo duas vezes, além de ser a única pessoa a ser premiada em dois campos científicos diferentes. Ela teve papel fundamental no legado da família Curie, de cinco prêmios Nobel. Ela também foi a primeira mulher a se tornar professora na Universidade de Paris e, em 1995, se tornou a primeira mulher a ser sepultada por seus próprios méritos no Panteão de Paris.</p>
+
+        <Botao nome="Conhecer mais sobre a gente"/>
+      </div>
+      <div className="div3">
+        <h1>+ OPORTUNIDADES </h1>
+        <p> Acreditamos que, o que separa grandes sonhos de grandes realizações, são oportunidades. Por isso, oferecemos um espaço dedicado a reunir cursos e vagas de trabalho afirmativas, especialmente voltados para mulheres na tecnologia. Nosso compromisso com a equidade e a inclusão impulsiona nossa missão de proporcionar um ambiente acolhedor e igualitário. Aqui você encontrará oportunidades de aprendizado e crescimento profissional.</p>
+
+        <Botao nome="Conhecer mais sobre a gente"/>
+      </div>
+      <div className="Foot">
+        FOOTER
+      </div>
+    </section>
+  );
+}
+
+function Botao(props) {
+  return (
+    <button>
+      {props.nome}
+    </button>
+  )
+}
+
+function SearchBar() {
+  return (
+    <div className="main">
+      <div className="search">
+        <TextField
+          id="outlined-basic"
+          variant="outlined"
+          fullWidth
+          label="Procure Aqui"
         />
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+    </div>
+  )
 }
