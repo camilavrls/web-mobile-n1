83f9ad2f86d5d77d8d117bc3dcb8be86e34bea5b
Camila Sousa <camilamorgada7@gmail.com>
2024-04-20 23:39:19 -0300

    adicionando sobre nos

--- src/app/sobrenos/page.jsx
+++ src/app/sobrenos/page.jsx
@@ -1,7 +1,22 @@
+import "./sobrenos.css"
+
 export default function SobreNos(){
     return(
         <div>
-            Sobre nós
+            <h1>SOBRE NÓS</h1>
+            <div className="sobrenos">
+            Bem-vindas à women's technology!<br/>
+            <br/>
+            Somos Camila, Maria e Isabelle, três amigas apaixonadas pela área de Tecnologia da Informação. Movidas pela nossa própria experiência e conscientes do desafio que é enfrentar a desigualdade de gênero no ambiente profissional, decidimos unir forças para criar este espaço acolhedor e inclusivo.<br/>
+            <br/>
+            Aqui, buscamos oferecer um refúgio onde mulheres na tecnologia possam encontrar apoio mútuo, solidariedade e inspiração. Acreditamos firmemente que cada obstáculo que enfrentamos é uma oportunidade de crescimento, e estamos comprometidas em compartilhar nossas jornadas, superando barreiras juntas e alcançando nossos objetivos.<br/>
+            <br/>
+            Celebramos a diversidade de habilidades e experiências, reconhecendo o valor único que cada uma traz para esta comunidade. Seja para compartilhar conhecimento, buscar orientação ou simplesmente encontrar um ombro amigo, estamos aqui para fortalecer e empoderar umas às outras.<br/>
+            <br/>
+            Juntas, somos mais fortes. Juntas, somos capazes de conquistar o mundo da tecnologia e inspirar as gerações futuras. Junte-se a nós nesta jornada! <br/>
+            <br/>
+            Obrigada por fazer parte da nossa comunidade.
+            </div>
         </div>
     );
 }
